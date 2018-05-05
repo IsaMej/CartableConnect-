ed15922f95634a37a955a39546130a1c23ce246e
IsaMej <isabelle.mejias.martin@gmail.com>
2018-05-05 09:04:10 +0200

    init connect bdd et index

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include ('includes/db_connect.inc.php');
+//include ('GestionPanda.php');
+$db=db_connect();
+
+    echo "Bienvenue sur ton cartable connecté ! </br></br>";
+ 
+    /*echo "Indiquez et modifiez l'état de vos comptes : </br></br>";
+    createAccount();
+    deleteAccount();
+    updateAccount();
+    createOp();*/
+
+
+?>
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PAccueil - Mon Cartable Connecté/title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="includes/index.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+        <div id=what>
+
+        </div>
+</body>
