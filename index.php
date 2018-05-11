1a2ff5c6f67b4468b638c389bf74696cfaa84f4b
nicolas <n.hamelin44120@gmail.com>
2018-05-11 14:17:34 +0200

    v1 dev

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-include ('includes/db_connect.inc.php');
-include ('GestionPanda.php');
-$db=db_connect();
+//include ('includes/db_connect.inc.php');
+//include ('GestionPanda.php');
+//$db=db_connect();
 
-    echo "Bienvenue sur ton cartable connecté ! </br></br>";
+   // echo "Bienvenue sur ton cartable connecté ! </br></br>";
  
     /*echo "Indiquez et modifiez l'état de vos comptes : </br></br>";
     createAccount();
@@ -12,21 +12,44 @@ $db=db_connect();
     createOp();*/
 
 
-?>
+?> 
 
 <!DOCTYPE <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>PAccueil - Mon Cartable Connecté/title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="includes/index.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    
-        <div id=what>
 
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Accueil - Mon Cartable Connecté</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="includes/index.css" />
+        <script src="main.js"></script>
+    </head>
+
+    <body>
+        <div id="container">
+
+            <header>
+                <img src="includes/logo.png" id="logo">
+                <h1></br>Plateforme de test - Mon cartable connecté</h1>
+            </header>
+
+            <div id="interface">
+                <article id="testDebit">
+                    <h2>Test du débit internet</h2>
+                </article>
+                <article id="testVideo">
+                    <h2>Test de la qualité vidéo</h2>
+                </article>
+                <article id="testSon">
+                    <h2>Test du son</h2>
+                </article>
+            </div>
+
+            <footer>
+                Copyright - <a href="http://www.moncartableconnecte.fr/" target ="_blank">Le cartable connecté</a>
+            </footer>
         </div>
-</body>
+    </body>
+
+</html>
