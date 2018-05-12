b190da239e99ba63824d0360a9a21292e79523f4
IsaMej <isabelle.mejias.martin@gmail.com>
2018-05-12 04:48:14 +0200

    astuces réso pb connexion

--- tests.php
+++ tests.php
@@ -35,23 +35,26 @@
         <div class="container-tests">
              <div class="bandeau-debit">
                 <?php
-                    echo "Test de la bande passante </br></br>";
-                    echo "Résultat : Le débit de connexion est trop lent.<br/>";
+                    echo "<h3>Test de la bande passante </h3></br></br>";
+                    
+                    echo "Résultat : Le débit de connexion est trop lent.</br></br>";
+                    echo '<img src="includes/images/astuces.png" id="astuces">';
                     echo "Astuces 1 : Eloigne le modem des sources de chaleur comme les radiateurs et dépoussière-le.
                                         Place-le modem près des équipements informatiques.</br></br>";
+                    echo "Astuces 2 : Assure toi que les câbles ne soient ni tordus ni abîmés.</br></br>";
                 ?>
             </div>
                     
             <div class="bandeau-miroir">
                 <?php
-                    echo "Test vidéo en miroir.</br></br>";
+                    echo "<h3>Test vidéo en miroir </3></br></br>";
                     echo "Vois-tu ton image appraître en double sur l'écran? </br></br>";
                 ?>
             </div>
 
             <div class="bandeau-video">
                 <?php
-                    echo "Test vidéo avec ta classe ! </br></br>"
+                    echo "<h3>Test vidéo avec ta classe !</3> </br></br>"
                 ?>
             </div>
         </div>
