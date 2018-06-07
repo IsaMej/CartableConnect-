c7c3918d64cdec2b98a8358c934df3bcb76d7850
Unknown <n.hamelin44120@gmail.com>
2018-06-07 11:26:22 +0200

    bootstrap

---
+++ tests1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Mon cartable connecté</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+		<link rel="stylesheet" type="text/css" href="./vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="./public/css/css.css">
+    </head>
+
+    <body>
+		<header>
+			<nav class="navbar navbar-default">
+				<div class="row">
+					<div class="col-md-4 col-md-offset-4">
+						<img src="./public/images/logoV2.svg" />
+					</div>
+				</div>
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						</button>
+					</div>
+					
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Accueil<span class="sr-only"></span></a></li>
+							<li><a href="#">Outils</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>
+        <div class="container">
+			<div class="container-inner">
+				<div class="row">
+					<div class="col-md-12">
+						<div id="connectionTest" class="carousel slide" data-ride="carousel" data-interval="false">
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner">
+								<div class="item active">
+									<p>
+										content
+									</p>
+								</div>
+
+								<div class="item">
+								</div>
+
+								<div class="item">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+
+		<script src="./vendor/jquery/jquery-3.3.1.min.js"></script>
+		<script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+
+</html>
