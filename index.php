c68c75d5216dadfd0b5f1e295f239c2d27339d60
LordOurs <alexandre.ochman@gmail.com>
2018-06-14 15:07:52 +0200

    index connexion db

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+    session_start();
+
+    function db_connect() {
+		$host = "localhost";
+		$dbname = "cartable_connect";
+		$user = "root";
+		$password = "";
+		//$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+		$db = new PDO(
+					"mysql:host=$host;dbname=$dbname", 
+					$user, 
+					$password, 
+					array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::MYSQL_ATTR_DIRECT_QUERY=>true));
+		return $db;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -19,7 +37,7 @@
 						<form method="POST" class="form-signin">
 						<input type="text" class="form-control" name="email" placeholder="Email" required autofocus>
 						<input type="password" class="form-control" name="password" placeholder="Mot de passe" required>
-						<button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+						<button class="btn btn-lg btn-primary btn-block" name="submitForm" type="submit">Connexion</button>
 						<label class="checkbox pull-left">
 							<input type="checkbox" value="remember-me">Se souvenir de moi ?
 						</label>
@@ -29,20 +47,19 @@
 			</div>
 		</div>
 		<?php
-			function formFunc() 
+			if(isset($_POST['submitForm']))
 			{
-				if(isset($_POST['submitForm']))
+				$db = db_connect();
+				$req = $db->prepare("SELECT * FROM user WHERE email = :email");
+				$req->execute(array("email" => $_POST['email']));
+				while($data = $req->fetch())
 				{
-					$db = db_connect();
-					$req = $db->prepare("SELECT * FROM users WHERE email_user = :email");
-					$req->execute(array("email" => $_POST['email']));
-					while($data = $req->fetch())
+					if($data['password'] == $_POST['password'])
 					{
-						if($data['pwd_user'] == $_POST['password'])
-						{
-							header("Location: tests.php");
-							exit;
-						}
+						$_SESSION['email']= $data['email'];
+						$_SESSION['id_user'] = $data['id_user'];
+						header("Location: tests.php");
+						exit;
 					}
 				}
 			}
